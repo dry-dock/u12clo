0c5d468aae226a300ad9fb0949447e5d5b462cb6
harsh157 <harsh12213@gmail.com>
2015-12-22 17:19:57 +0530

    Change tmp to u12clo in Dockerfile and in install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+
+sudo apt-get clean
+sudo mv /var/lib/apt/lists /tmp
+sudo mkdir -p /var/lib/apt/lists/partial
+sudo apt-get clean
+sudo apt-get update
+
 # install Clojure's build tool: leiningen
 echo "================= Install clojure's build tool: leiningen ==================="
 sudo wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
 sudo mv lein /usr/local/bin/lein
 sudo chmod a+x /usr/local/bin/lein
 
-for file in /tmp/version/*;
+for file in /u12clo/version/*;
 do
   $file
 done
